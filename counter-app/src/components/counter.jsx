0b932a0955a152c777f52705bcc2c0e329567ed6
Jihani-31 <jihaninanayakkara@gmail.com>
2021-05-25 14:08:03 +0530

    embedding expressions

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -1,21 +1,26 @@
 import React, { Component } from 'react'
 class Person extends Component {
+    state={
+        count:1
+    };
     
     render() { 
-      
-        
          return (
             <React.Fragment>
-                <h1>Hello world </h1>
+                <span>{this.formatCount()} </span>
                 <button>Increment</button>
             </React.Fragment>
 
 
          ) ;
-         
-         
-
     }
+    formatCount(){
+        const {count}=this.state;  //assign this.state to count
+        return count === 0 ? <h1>Zero</h1> :<h1>{count}</h1>; //if count equal to  zero then return "zero",otherwise return count 
+        
+    }
+
+
 }
  
 export default Person;
