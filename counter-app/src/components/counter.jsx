cd574f7241cc3278cc4e45e40af638430942b17f
Jihani-31 <jihaninanayakkara@gmail.com>
2021-05-25 20:01:44 +0530

    rendering

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 class Person extends Component {
-    state={
-        count:6,
+    state={   //state object
+        count:11,
         imageUrl:"https://picsum.photos/200"
     };
 
@@ -12,16 +12,23 @@ class Person extends Component {
     };
     
     render() { 
+      //  let classes = this.getBadgeClasses(); //used control+shift+r to extract the method
          return (
             <div>
                 <img src={this.state.imageUrl} alt="" />
-                <span style={this.style} className="badge badge-primary m-2">{this.formatCount()} </span>
+                <span style={this.style} className={this.getBadgeClasses()}>{this.formatCount()} </span>
                 <button className="btn btn-danger btn-sm">Increment</button>
             </div>
 
 
          ) ;
     }
+    getBadgeClasses() {
+        let classes = "badge m-2 ";
+        classes += this.state.count === 0 ? "badge-warning" : "badge-primary"; //if count is equal to 0 then button collor is yellow,otherwise button color is blue
+        return classes;
+    }
+
     formatCount(){
         const {count}=this.state;  //assign this.state to count
         return count === 0 ? <h1>Zero</h1> :<h1>{count}</h1>; //if count equal to  zero then return "zero",otherwise return count 
