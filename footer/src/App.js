c07063ab3b366429f90918e1ee9c98fced512e1e
Jihani-31 <jihaninanayakkara@gmail.com>
2021-06-27 02:44:19 +0530

    footer create

---
+++ footer/src/App.js
@@ -0,0 +1,11 @@
+// import logo from './logo.svg';
+// import './App.css';
+import React, { Component } from 'react';
+
+function App() {
+  return (
+    <h1>Yoooo</h1>
+   );
+}
+
+export default App;
